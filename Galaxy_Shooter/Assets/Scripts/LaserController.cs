5ee467f50d0a57270cd376d00adf88df05cb8a5f
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2018-06-26 00:28:05 +0530

    Added laser controller script

---
+++ Galaxy_Shooter/Assets/Scripts/LaserController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    // Editor variables
+    public float speed = 1.0f;
+
+    [SerializeField]
+    private float visibleBoundary = 10.0f;
+    [SerializeField]
+    private bool debug = false;
+
+    // Initialize
+    void Start ()
+    {
+        // Debug message
+        if (debug)
+        {
+            Debug.Log("[INFO] LaserController initialized");
+        }
+    }
+
+    // Update
+    void Update ()
+    {
+        // Move laser
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        // Destroy laser if out of visible boundary
+        if (transform.position.y > visibleBoundary)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
