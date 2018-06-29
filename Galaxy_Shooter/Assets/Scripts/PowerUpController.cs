a460cfa238b248abecb49a2e7d8894a9d5eecd5f
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2018-06-29 23:48:05 +0530

    Updated components

---
+++ Galaxy_Shooter/Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,39 @@
+﻿// Power up controller.
+// Created on Fri Jun 29 22:00:00 2018
+// Author: Prasun Roy (https://github.com/prasunroy)
+// GitHub: https://github.com/prasunroy/galaxy-shooter
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour
+{
+    [SerializeField]
+    private bool _debug = false;
+    [SerializeField]
+    private float _speed = 1.0f;
+
+    // Initialize
+    void Start ()
+    {
+        // Debug message
+        if (_debug)
+        {
+            Debug.Log("[INFO] PowerUpController initialized");
+        }
+    }
+    
+    // Update
+	void Update ()
+    {
+        // Move power up
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    // Triggered collision
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // ACTION
+    }
+}
